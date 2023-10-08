130465d2ea1ee920dd70de21479c7992b6604d9e
BhavinShah0105 <bhavinshah0105@gmail.com>
2023-10-08 08:04:10 +0530

    Login page Design

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,50 +1,25 @@
 import React from "react";
-import { Property1Hide } from "../../icons/Property1Hide";
-import "./style.css";
+import logo from "../../img/Logo.png";
+import hexagons from "../../img/login_hexagons@2x.png";
+import cloud from "../../img/login_cloud-storage.png"
+
+import "./login_style.css";
 
 export const Login = () => {
   return (
     <div className="frame-login">
       <div className="div">
-        <div className="overlap-group">
-          <div className="lines">
-            <img className="line" alt="Line" src="https://c.animaapp.com/nU8qvlv2/img/line-205.svg" />
-            <img className="img" alt="Line" src="https://c.animaapp.com/nU8qvlv2/img/line-205.svg" />
-          </div>
-          <div className="text-wrapper">Or</div>
-        </div>
-        <p className="new-member-create">
-          <span className="span">New member? </span>
-          <span className="text-wrapper-2">Create Account</span>
-        </p>
-        <div className="frame">
-        <div className="text-wrappers-3">
-        <button type="submit">Login</button>
-      </div>
-      
-        </div>
-        <div className="frame-3">
-        <input type="text" placeholder="Work email *" className="text-wrapper-6" />
-      </div>
-      
-      <div className="frame-2">
-        <input type="password" placeholder="Password *" className="text-wrapper-5" />
-        <Property1Hide className="password-hide-and" color="#1B2547" opacity="0.22" />
-      </div>
-      
-      <div className="div-wrapper">
-        <input type="password" placeholder="Confirm password *" className="text-wrapper-4" />
-      </div>
-z      
+        <img className="image" alt="Image" src={logo} />
         <div className="overlap">
-          <img className="hexagons" alt="Hexagons" src="https://c.animaapp.com/nU8qvlv2/img/hexagons@2x.png" />
-          <img className="cloud-storage-image" alt="Cloud storage image" src="https://c.animaapp.com/nU8qvlv2/img/cloud-storage-image@2x.png"/>
+          <img className="hexagons" alt="Hexagons" src={hexagons} />
+          <img className="cloud-storage-image" alt="Cloud storage image" src={cloud} />
           <div className="ellipse" />
         </div>
         <div className="ellipse-2" />
         <div className="ellipse-3" />
-        <div className="text-wrapper-7">Login</div>
-        <img className="image" alt="Image" src="https://c.animaapp.com/nU8qvlv2/img/image001-1@2x.png" />
+
+        {/*form code*/}
+
       </div>
     </div>
   );
